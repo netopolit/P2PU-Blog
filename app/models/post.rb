4b0b2671cc80de09680b3b49f23fed207f75fca7
Mislav Paparella <mpaparel@me.com>
2011-02-13 04:30:40 +0100

    vanilla RailsGuides blog

---
+++ app/models/post.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+# Schema version: 20110213015550
+#
+# Table name: posts
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Post < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :title, :presence => true,
+                    :length => {:minimum => 5}
+
+  has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  
+end
