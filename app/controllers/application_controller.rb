026d67d038660aeeefc4652c78c5373ec84507f3
Mislav Paparella <mpaparel@me.com>
2011-02-17 00:54:56 +0100

    removed default authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
- 
-  private
- 
-  def authenticate
-    authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == 'mislav' && password == 'password'
-    end
-  end
+
  
 end
