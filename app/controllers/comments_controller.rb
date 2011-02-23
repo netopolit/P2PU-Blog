c3aef18f00eca151f01ea0d7f584ccc39a17c42a
Mislav Paparella <mpaparel@me.com>
2011-02-23 06:34:18 +0100

    ajaxified new comment form/new comment display

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,14 +7,20 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post) }
+      format.js
+    end
   end
  
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post) }
+      format.js
+    end
   end
 
   private
